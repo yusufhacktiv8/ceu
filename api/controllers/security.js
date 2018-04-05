e2bb84da38dc2e1f7c890e21ddbc7719af83c889
myyusuf <myyusuf1911@gmail.com>
2018-04-05 23:21:52 +0700

    Fix security

--- api/controllers/security.js
+++ api/controllers/security.js
@@ -1,13 +1,12 @@
 const jwt = require('jsonwebtoken');
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 const models = require('../models');
-const Constant = require('../Constant');
 
-const sendLoginFailedMessage = function(req, res) {
+const sendLoginFailedMessage = function (req, res) {
   res.send('Invalid username or password', 403);
 };
 
-exports.signIn = function(req, res) {
+exports.signIn = function (req, res) {
   const username = req.body.username;
   const password = req.body.password;
 
@@ -20,31 +19,20 @@ exports.signIn = function(req, res) {
     ],
   })
   .then((user) => {
-    if (user !== null && user.Role !== null) {
-      // bcrypt.compare(password, user.password, function(err, bcryptResult) {
-      //   if (bcryptResult) {
-      //     const token = jwt.sign({
-      //       name: user.name,
-      //       role: user.role
-      //     }, TOKEN_PASSWORD);
-      //     res.send(token);
-      //   } else {
-      //     sendLoginFailedMessage(req, res);
-      //   }
-      // });
-      if (password === user.password) {
-        const token = jwt.sign({
-          name: user.name,
-          role: user.Role.code,
-        }, Constant.TOKEN_PASSWORD);
-        res.json({
-          status: 'OK',
-          token,
-          role: user.Role.code,
-        });
-      } else {
-        sendLoginFailedMessage(req, res);
-      }
+    if (user !== null) {
+      bcrypt.compare(password, user.password, (err, bcryptResult) => {
+        if (bcryptResult) {
+          const token = jwt.sign({
+            name: user.name,
+            roles: user.roles,
+          }, process.env.REACT_APP_TOKEN_PASSWORD, { expiresIn: 60 * 30 });
+          res.json({
+            token,
+          });
+        } else {
+          sendLoginFailedMessage(req, res);
+        }
+      });
     } else {
       sendLoginFailedMessage(req, res);
     }
