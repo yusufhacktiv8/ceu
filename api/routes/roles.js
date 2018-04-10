7a132b5406c4652d729e5139dbb690ecef94c08b
myyusuf <myyusuf1911@gmail.com>
2018-04-10 22:35:59 +0700

    Fix security

--- api/routes/roles.js
+++ api/routes/roles.js
@@ -5,9 +5,9 @@ const { isAuthorizedAs } = require('../helpers/AuthUtils');
 const router = express.Router();
 
 router.get('/', isAuthorizedAs('ADMIN'), RoleController.findAll);
-router.get('/:roleId', RoleController.findOne);
-router.post('/', RoleController.create);
-router.put('/:roleId', RoleController.update);
-router.delete('/:roleId', RoleController.destroy);
+router.get('/:roleId', isAuthorizedAs('ADMIN'), RoleController.findOne);
+router.post('/', isAuthorizedAs('ADMIN'), RoleController.create);
+router.put('/:roleId', isAuthorizedAs('ADMIN'), RoleController.update);
+router.delete('/:roleId', isAuthorizedAs('ADMIN'), RoleController.destroy);
 
 module.exports = router;
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const UserController = require('../controllers/users.js');
+const { isAuthorizedAs } = require('../helpers/AuthUtils');
 
 const router = express.Router();
 
-router.get('/', UserController.findAll);
-router.get('/:userId', UserController.findOne);
-router.post('/', UserController.create);
-router.put('/:userId', UserController.update);
-router.delete('/:userId', UserController.destroy);
+router.get('/', isAuthorizedAs('ADMIN'), UserController.findAll);
+router.get('/:userId', isAuthorizedAs('ADMIN'), UserController.findOne);
+router.post('/', isAuthorizedAs('ADMIN'), UserController.create);
+router.put('/:userId', isAuthorizedAs('ADMIN'), UserController.update);
+router.delete('/:userId', isAuthorizedAs('ADMIN'), UserController.destroy);
 
 module.exports = router;
