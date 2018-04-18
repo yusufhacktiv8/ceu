2bd5e25f1b36b384f1a8f1bcc6458a2ff11f36d6
myyusuf <myyusuf1911@gmail.com>
2018-04-18 23:17:20 +0700

    Fix department security

--- api/routes/department.js
+++ api/routes/department.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const router = express.Router();
+const { isAuthorizedAs } = require('../helpers/AuthUtils');
 const DepartmentController = require('../controllers/departments.js');
 
-router.get('/', DepartmentController.findAll);
-router.post('/', DepartmentController.create);
-router.get('/:departmentId', DepartmentController.findOne);
-router.put('/:departmentId', DepartmentController.update);
-router.delete('/:departmentId', DepartmentController.delete);
+router.get('/', isAuthorizedAs('ADMIN'), DepartmentController.findAll);
+router.post('/', isAuthorizedAs('ADMIN'), DepartmentController.create);
+router.get('/:departmentId', isAuthorizedAs('ADMIN'), DepartmentController.findOne);
+router.put('/:departmentId', isAuthorizedAs('ADMIN'), DepartmentController.update);
+router.delete('/:departmentId', isAuthorizedAs('ADMIN'), DepartmentController.delete);
 
 module.exports = router;
