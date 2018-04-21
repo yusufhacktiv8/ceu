c850e22b5a2897fb63fe614966897ae1c76a3012
myyusuf <myyusuf1911@gmail.com>
2018-04-21 09:06:49 +0700

    Fetch course

--- admin/src/student/course/details/CourseDetailsPage.jsx
+++ admin/src/student/course/details/CourseDetailsPage.jsx
@@ -1,21 +1,55 @@
 import React, { Component } from 'react';
-import { Layout, Tabs } from 'antd';
+import { Layout, Tabs, Spin, Icon, Tag } from 'antd';
+import axios from 'axios';
 import InfoForm from './InfoForm';
+import showError from '../../../utils/ShowError';
 
 const { Header, Content } = Layout;
 const TabPane = Tabs.TabPane;
 
+const COURSES_URL = `${process.env.REACT_APP_SERVER_URL}/api/courses`;
+const spinIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />;
+
 export default class CourseDetailsPage extends Component {
   state = {
     course: {},
+    loading: false,
   }
+  componentDidMount() {
+    this.fetchCourse();
+  }
+
   goToStudentPage = () => {
     this.props.history.push('/students');
   }
+
   goToStudentDetailsPage = () => {
     const { match } = this.props;
     const { studentId } = match.params;
     this.props.history.push(`/students/${studentId}`);
+  }
+
+  fetchCourse = () => {
+    const { match } = this.props;
+    const { courseId } = match.params;
+    this.setState({
+      loading: true,
+    });
+    axios.get(`${COURSES_URL}/${courseId}`, {})
+      .then((response) => {
+        this.setState({
+          course: response.data,
+          loading: false,
+        });
+      })
+      .catch((error) => {
+        showError(error);
+      })
+      .finally(() => {
+        this.setState({
+          loading: false,
+        });
+      });
   }
   render() {
     return (
@@ -26,6 +60,25 @@ export default class CourseDetailsPage extends Component {
           <span className="page-header-title"> Course</span>
         </Header>
         <Content className="page-content">
+          {
+            this.state.loading ?
+              (
+                <Spin indicator={spinIcon} style={{ marginBottom: 15 }} />
+              ) :
+              (
+                <div>
+                  <Tag color="#F50" style={{ height: 26, marginBottom: 15, fontSize: 15 }}>
+                    {this.state.course.Student ? this.state.course.Student.name : ''}
+                  </Tag>
+                  <Tag style={{ height: 26, marginBottom: 15, fontSize: 15 }}>
+                    {this.state.course.Student ? `${this.state.course.Student.oldSid} - ${this.state.course.Student.newSid}` : ''}
+                  </Tag>
+                  <Tag style={{ height: 26, marginBottom: 15, fontSize: 15 }}>
+                    {this.state.course.Department ? this.state.course.Department.code : ''}
+                  </Tag>
+                </div>
+              )
+          }
           <Tabs defaultActiveKey="1" style={{ marginTop: -10, height: 500 }}>
             <TabPane tab="Info" key="1">
               <InfoForm course={this.state.course} />
--- admin/src/student/course/details/InfoForm.jsx
+++ admin/src/student/course/details/InfoForm.jsx
@@ -3,15 +3,15 @@ import { Form, Input, Row, Col } from 'antd';
 
 const FormItem = Form.Item;
 
-const InfoForm = ({ form }) => {
-  const { getFieldDecorator, title } = form;
+const InfoForm = ({ form, course }) => {
+  const { getFieldDecorator } = form;
   return (
     <Form layout="vertical">
       <Row>
         <Col span={12}>
           <FormItem label="Title">
             {getFieldDecorator('title', {
-              initialValue: title,
+              initialValue: course.title,
               rules: [
                 { required: true, message: 'Please input title' },
               ],
