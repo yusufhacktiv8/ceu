9bb3d2cea37e83d3881a6224a2ec11b38dea4c50
yusufhacktiv8 <yusuf.hacktiv8@gmail.com>
2018-08-11 07:41:46 +0700

    Fix assistance

--- admin/src/assistance/AssistanceWindow.jsx
+++ admin/src/assistance/AssistanceWindow.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import { Modal, Form, Input, InputNumber, DatePicker, TimePicker, Button, Row, Col, message } from 'antd';
+import { Modal, Form, Input, InputNumber, Checkbox, DatePicker, TimePicker, Button, Row, Col, Tabs, message } from 'antd';
 import axios from 'axios';
 import moment from 'moment';
 import showError from '../utils/ShowError';
 import { dateFormat, timeFormat } from '../constant';
 import DepartmentSelect from '../settings/department/DepartmentSelect';
 import AssistanceTopicSelect from '../settings/assistance_topic/AssistanceTopicSelect';
-import SupervisorSelect from '../settings/supervisor/SupervisorSelect';
+import TutorSelect from '../settings/tutor/TutorSelect';
 
 const ASSISTANCES_URL = `${process.env.REACT_APP_SERVER_URL}/api/assistances`;
 
 const FormItem = Form.Item;
+const { TabPane } = Tabs;
 
 class AssistanceWindow extends Component {
   state = {
@@ -69,118 +70,193 @@ class AssistanceWindow extends Component {
         ]}
       >
         <Form layout="vertical">
-          <FormItem label="Code">
-            {getFieldDecorator('code', {
-              initialValue: assistance.code,
-              rules: [
-                { required: true, message: 'Please input code' },
-              ],
-            })(
-              <Input maxLength="30" />,
-            )}
-          </FormItem>
-          <FormItem label="Name">
-            {getFieldDecorator('name', {
-              initialValue: assistance.name,
-              rules: [
-                { required: true, message: 'Please input name' },
-              ],
-            })(
-              <Input maxLength="50" />,
-            )}
-          </FormItem>
-          <Row gutter={10}>
-            <Col span={12}>
-              <FormItem label="Department">
-                {getFieldDecorator('department', {
-                  initialValue: assistance.AssistanceTopic ? String(assistance.AssistanceTopic.DepartmentId) : undefined,
+          <Tabs
+            defaultActiveKey="1"
+            style={{ marginTop: -15, height: 508 }}
+          >
+            <TabPane tab="Main" key="1">
+              <FormItem label="Code">
+                {getFieldDecorator('code', {
+                  initialValue: assistance.code,
                   rules: [
-                    { required: true, message: 'Please input department' },
+                    { required: true, message: 'Please input code' },
                   ],
                 })(
-                  <DepartmentSelect level={-1} />,
+                  <Input maxLength="30" />,
                 )}
               </FormItem>
-            </Col>
-            <Col span={12}>
-              <FormItem label="Assistance Topic">
-                {getFieldDecorator('assistanceTopic', {
-                  initialValue: assistance.AssistanceTopic ? assistance.AssistanceTopic.id : undefined,
+              <FormItem label="Name">
+                {getFieldDecorator('name', {
+                  initialValue: assistance.name,
                   rules: [
-                    { required: true, message: 'Please input assistance topic' },
+                    { required: true, message: 'Please input name' },
                   ],
                 })(
-                  <AssistanceTopicSelect department={form.getFieldValue('department')} />,
+                  <Input maxLength="50" />,
                 )}
               </FormItem>
-            </Col>
-          </Row>
-          <FormItem label="Batch">
-            {getFieldDecorator('batch', {
-              initialValue: assistance.batch,
-              rules: [
-                { required: true, message: 'Please input batch' },
-              ],
-            })(
-              <InputNumber min={1} max={4} />,
-            )}
-          </FormItem>
-          <FormItem label="Duration (Minutes)">
-            {getFieldDecorator('duration', {
-              initialValue: assistance.duration,
-              rules: [
-                { required: true, message: 'Please input duration' },
-              ],
-            })(
-              <InputNumber min={1} max={600} />,
-            )}
-          </FormItem>
-          <Row gutter={10}>
-            <Col span={12}>
-              <FormItem label="Date">
-                {getFieldDecorator('eventDate', {
-                  initialValue: assistance.eventDate ? moment(assistance.eventDate) : undefined,
+              <Row gutter={10}>
+                <Col span={12}>
+                  <FormItem label="Department">
+                    {getFieldDecorator('department', {
+                      initialValue: assistance.AssistanceTopic ? String(assistance.AssistanceTopic.DepartmentId) : undefined,
+                      rules: [
+                        { required: true, message: 'Please input department' },
+                      ],
+                    })(
+                      <DepartmentSelect level={-1} />,
+                    )}
+                  </FormItem>
+                </Col>
+                <Col span={12}>
+                  <FormItem label="Assistance Topic">
+                    {getFieldDecorator('assistanceTopic', {
+                      initialValue: assistance.AssistanceTopic ? assistance.AssistanceTopic.id : undefined,
+                      rules: [
+                        { required: true, message: 'Please input assistance topic' },
+                      ],
+                    })(
+                      <AssistanceTopicSelect department={form.getFieldValue('department')} />,
+                    )}
+                  </FormItem>
+                </Col>
+              </Row>
+              <FormItem label="Batch">
+                {getFieldDecorator('batch', {
+                  initialValue: assistance.batch,
                   rules: [
-                    { required: true, message: 'Please input date' },
+                    { required: true, message: 'Please input batch' },
                   ],
                 })(
-                  <DatePicker />,
+                  <InputNumber min={1} max={4} />,
                 )}
               </FormItem>
-            </Col>
-            <Col span={12}>
-              <FormItem label="Time">
-                {getFieldDecorator('eventTime', {
-                  initialValue: assistance.eventTime ? moment(assistance.eventTime, timeFormat) : undefined,
+              <FormItem label="Duration (Minutes)">
+                {getFieldDecorator('duration', {
+                  initialValue: assistance.duration,
                   rules: [
-                    { required: true, message: 'Please input time' },
+                    { required: true, message: 'Please input duration' },
                   ],
                 })(
-                  <TimePicker />,
+                  <InputNumber min={1} max={600} />,
                 )}
               </FormItem>
-            </Col>
-          </Row>
-          <Row gutter={10}>
-            <Col span={12}>
-              <FormItem label="Speaker">
-                {getFieldDecorator('speaker', {
-                  initialValue: assistance.speakerId || undefined,
-                })(
-                  <SupervisorSelect />,
-                )}
-              </FormItem>
-            </Col>
-            <Col span={12}>
-              <FormItem label="Moderator">
-                {getFieldDecorator('moderator', {
-                  initialValue: assistance.moderatorId || undefined,
-                })(
-                  <SupervisorSelect />,
-                )}
-              </FormItem>
-            </Col>
-          </Row>
+              <Row gutter={10}>
+                <Col span={12}>
+                  <FormItem label="Date">
+                    {getFieldDecorator('eventDate', {
+                      initialValue: assistance.eventDate ? moment(assistance.eventDate) : undefined,
+                      rules: [
+                        { required: true, message: 'Please input date' },
+                      ],
+                    })(
+                      <DatePicker />,
+                    )}
+                  </FormItem>
+                </Col>
+                <Col span={12}>
+                  <FormItem label="Time">
+                    {getFieldDecorator('eventTime', {
+                      initialValue: assistance.eventTime ? moment(assistance.eventTime, timeFormat) : undefined,
+                      rules: [
+                        { required: true, message: 'Please input time' },
+                      ],
+                    })(
+                      <TimePicker />,
+                    )}
+                  </FormItem>
+                </Col>
+              </Row>
+            </TabPane>
+            <TabPane tab="Speaker" key="2">
+              <Row>
+                <Col span={16}>
+                  <FormItem label="Utama">
+                    {getFieldDecorator('mainTutor', {
+                      initialValue: assistance.mainTutorId || undefined,
+                    })(
+                      <TutorSelect />,
+                    )}
+                  </FormItem>
+                </Col>
+                <Col span={8}>
+                  <FormItem label="">
+                    {getFieldDecorator('mainTutorPresent', {
+                      initialValue: assistance.mainTutorPresent,
+                      valuePropName: 'checked',
+                    })(
+                      <Checkbox>Present</Checkbox>,
+                    )}
+                  </FormItem>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={16}>
+                  <FormItem label="Cadangan">
+                    {getFieldDecorator('secondTutor', {
+                      initialValue: assistance.secondTutorId || undefined,
+                    })(
+                      <TutorSelect />,
+                    )}
+                  </FormItem>
+                </Col>
+                <Col span={8}>
+                  <FormItem label="">
+                    {getFieldDecorator('secondTutorPresent', {
+                      initialValue: assistance.secondTutorPresent,
+                      valuePropName: 'checked',
+                    })(
+                      <Checkbox>Present</Checkbox>,
+                    )}
+                  </FormItem>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={16}>
+                  <FormItem label="Siaga">
+                    {getFieldDecorator('thirdTutor', {
+                      initialValue: assistance.thirdTutorId || undefined,
+                    })(
+                      <TutorSelect />,
+                    )}
+                  </FormItem>
+                </Col>
+                <Col span={8}>
+                  <FormItem label="">
+                    {getFieldDecorator('thirdTutorPresent', {
+                      initialValue: assistance.thirdTutorPresent,
+                      valuePropName: 'checked',
+                    })(
+                      <Checkbox>Present</Checkbox>,
+                    )}
+                  </FormItem>
+                </Col>
+              </Row>
+            </TabPane>
+            <TabPane tab="Moderator" key="3">
+              <Row>
+                <Col span={16}>
+                  <FormItem label="Moderator">
+                    {getFieldDecorator('facilitator', {
+                      initialValue: assistance.facilitatorId || undefined,
+                    })(
+                      <TutorSelect />,
+                    )}
+                  </FormItem>
+                </Col>
+                <Col span={8}>
+                  <FormItem label="">
+                    {getFieldDecorator('facilitatorPresent', {
+                      initialValue: assistance.facilitatorPresent,
+                      valuePropName: 'checked',
+                    })(
+                      <Checkbox>Present</Checkbox>,
+                    )}
+                  </FormItem>
+                </Col>
+              </Row>
+            </TabPane>
+          </Tabs>
         </Form>
       </Modal>
     );
