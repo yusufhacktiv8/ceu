0b7aac80c0d9f959b94e67d310c48c40463cebf2
yusufhacktiv8 <yusuf.hacktiv8@gmail.com>
2019-02-24 21:19:26 +0700

    Add error log

--- api/app.js
+++ api/app.js
@@ -168,6 +168,7 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+  console.error(err);
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
